96115e29db6c01347b03d0c75a0f00eebd30ba7f
Luz Perdomo <lilyperdomo92@gmail.com>
2020-07-21 18:17:49 -0500

    added header file

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -11,12 +11,10 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-
-	if (!tree || !func)
+	if (tree == NULL || func == NULL)
 		return;
 
 	printf("%d\n", tree->n);
-
 	binary_tree_preorder(tree->left, func);
 	binary_tree_preorder(tree->right, func);
 }
